cebd37433e7f22ce47ab703548d5bf10b76d9ce9
Sohail <sohailkarmani16@gmail.com>
2024-06-25 18:16:39 +0500

    add checkprime file

---
+++ Looping/CheckPrime.java
@@ -0,0 +1,29 @@
+package Looping;
+
+import java.util.Scanner;
+
+public class CheckPrime {
+    static boolean checkPrime(int num) {
+        int count = 0;
+        for (int i = 1; i <= num; i++) {
+            if (num % i == 0) {
+                count++;
+            }
+        }
+        if (count == 2) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number : ");
+        int num = sc.nextInt();
+        System.out.println(checkPrime(num));
+        sc.close();
+    }
+
+}
