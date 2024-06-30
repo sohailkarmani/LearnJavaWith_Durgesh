be2bcb6b32ffb9db6b4328cd104ac512150df2a0
Sohail <sohailkarmani16@gmail.com>
2024-06-30 22:47:16 +0500

    file added

--- Pattern/Pattern1.java
+++ Pattern/Pattern1.java
@@ -6,9 +6,8 @@ public class Pattern1 {
 
     static void numberPattern(int num) {
         for (int i =1; i<=num; i++) {
-            for (int j =1; j<=num; j++) {
-                System.out.print(j + " ");
-                num--;
+            for (int j =1; j<=i; j++) {
+                System.out.print("*");
             }
             System.out.println();
             //num--;
