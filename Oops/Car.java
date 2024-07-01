403eb88f023ed1acd8b9c5584d22da4e36787fc0
Sohail <sohailkarmani16@gmail.com>
2024-07-01 22:36:48 +0500

    file added

---
+++ Oops/Car.java
@@ -0,0 +1,26 @@
+package Oops;
+
+abstract class Vehicle {
+    abstract void start(); 
+    
+}
+ 
+public class  Car extends Vehicle{
+    void start(){
+        System.out.println("Car starts with key");
+    }
+    
+}
+ 
+ class Scooter extends Vehicle {
+
+    void start(){
+        System.out.println("Scooter starts with kick");
+    }
+    public static void main(String[] args) {
+        Car c=new Car();
+        c.start();
+        Scooter s=new Scooter();
+        s.start();
+    }
+}
