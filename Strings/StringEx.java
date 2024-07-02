a168452178066d02d0e2f9d75675073882e620d4
Sohail <sohailkarmani16@gmail.com>
2024-07-02 22:30:49 +0500

    added

---
+++ Strings/StringEx.java
@@ -0,0 +1,15 @@
+package Strings;
+
+public class StringEx {
+    public static void main(String[] args) {
+        // created using : literal
+        String str="This is working fine";
+        // created using new keyword
+        String str1=new String("This is not working fine");
+        System.out.println(str);
+        System.out.println(str1);
+        // creating blank string
+        String srt=new String();
+        
+    }
+}
