c90e34d2bb1c542e55c576afa928b85737f8f65a
Sohail <sohailkarmani16@gmail.com>
2024-07-03 21:55:02 +0500

    added

---
+++ Strings/StringEx2.java
@@ -0,0 +1,14 @@
+package Strings;
+
+public class StringEx2 {
+    public static void main(String[] args) {
+        String str = "Iam a java developer from Pakistan";
+        System.out.println(str.length());
+        System.out.println(str.charAt(7));
+        System.out.println(str.substring(5));
+        System.out.println(str.contains("java"));
+        System.out.println(str.isBlank());
+        System.out.println(str.isEmpty());
+
+    }
+}
